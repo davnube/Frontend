7df4d59dcec756e032137db3417897f63de6ba02
davnube <dav.nube@gmail.com>
2023-05-08 20:43:06 -0600

    Update data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { HeaderComponent } from './commons/header/header.component';
+import { SectionComponent } from './commons/section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule
